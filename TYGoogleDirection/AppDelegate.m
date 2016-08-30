9239770b9cd354e3ca32ed36b550a39370ec5b10
Thabresh <vividtester@gmail.com>
2016-08-30 20:00:19 +0530

    Update AppDelegate.m

--- TYGoogleDirection/AppDelegate.m
+++ TYGoogleDirection/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:API_KEY];
     // Override point for customization after application launch.
     return YES;
 }
